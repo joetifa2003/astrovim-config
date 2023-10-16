fe8a45fe64226aa40978a550af9d560088924781
Youssef ahmed <youssef.cyza@gmail.com>
2023-10-16 08:38:18 +0300

    chore: remove unused plugins

--- plugins/themes.lua
+++ plugins/themes.lua
@@ -1,34 +1,9 @@
 return {
-  {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false,
-    config = function()
-      require("monokai-pro").setup {
-        filter = "ristretto",
-        styles = {
-          comment = { italic = true },
-          keyword = { italic = false }, -- any other keyword
-          type = { italic = false }, -- (preferred) int, long, char, etc
-          storageclass = { italic = false }, -- static, register, volatile, etc
-          structure = { italic = false }, -- struct, union, enum, etc
-          parameter = { italic = false }, -- parameter pass in function
-          annotation = { italic = false },
-          tag_attribute = { italic = false }, -- attribute of tag in reactjs
-        },
-      }
-    end,
-  },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
     config = function() require("kanagawa").setup() end,
-  },
-  {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function() require("nordic").load() end,
   },
   {
     "folke/tokyonight.nvim",
