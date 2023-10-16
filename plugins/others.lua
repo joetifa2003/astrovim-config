ada71702ac4c39396bf74a1453aa47b9b66f2861
Youssef ahmed <youssef.cyza@gmail.com>
2023-10-16 06:16:50 +0300

    add dap virtual text

--- plugins/others.lua
+++ plugins/others.lua
@@ -3,4 +3,13 @@ return {
   { "sudoerwx/vim-ray-so-beautiful", lazy = false },
   { "wakatime/vim-wakatime", lazy = false },
   -- { "joerdav/templ.vim", lazy = false },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function()
+      require("nvim-dap-virtual-text").setup {
+        virt_text_pos = "eol",
+      }
+    end,
+  },
 }
