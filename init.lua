d868ce51a760facca55895cd71527f924add6171
Youssef ahmed <youssef.cyza@gmail.com>
2023-10-16 06:15:32 +0300

    switch colorscheme

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 })
 
 return {
-  colorscheme = "monokai-pro",
+  colorscheme = "tokyonight-night",
   lsp = {
     servers = {
       "gopls",
