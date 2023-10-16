27deb057d8f7510fc0a546dd405b0b458a3e42bb
Youssef ahmed <youssef.cyza@gmail.com>
2023-10-16 08:40:30 +0300

    remove unused mapping

--- mappings.lua
+++ mappings.lua
@@ -4,9 +4,6 @@ return {
       function() require("neotest").summary.toggle() end,
       desc = "Toggle neotest summary",
     },
-    ["<leader>fj"] = {
-      function() vim.cmd "Farr" end,
-    },
     ["<leader>gg"] = {
       function() vim.cmd "LazyGit" end,
       desc = "Open lazygit",
